78d33705e19dcb4657f13d8bd8f05849fd6fcc66
jihui <topkst00@gmail.com>
2020-04-13 17:01:14 +0900

    출결앱 1/2 - 웹뷰띄우기 - FCM 푸시받기 - 소리내기

--- app/src/main/java/com/example/activity/MainActivity.java
+++ app/src/main/java/com/example/activity/MainActivity.java
@@ -1,11 +1,17 @@
-package com.example.exam_imentrance_njh;
+package com.example.activity;
 
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
+import android.Manifest;
 import android.annotation.TargetApi;
-import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -19,18 +25,24 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
 import android.webkit.PermissionRequest;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.BaseAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.exam_imentrance_njh.R;
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.messaging.FirebaseMessaging;
 
 import java.net.NetworkInterface;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -43,6 +55,8 @@ public class MainActivity extends AppCompatActivity {
     public String URL_DOMAIN; //webview주소
     public String URL_INDEX_FULL; //webview index full 주소
     public String URL_INDEX; //webview index 폴더 주소
+    public static final int REQUEST_ENABLE_BT = 1; //블루투스 실행권한 번호
+    public static final int REQUEST_ACCESS_COARSE_LOCATION = 2; // GPS 실행권한 번호
 
     private WebView wv_entrance;
     private String final_url; //최종 webview에 띄울 url
@@ -55,6 +69,12 @@ public class MainActivity extends AppCompatActivity {
     private boolean flag_wv_load_finish;  //webview_출결체크 종료 프레그
     public static String nfc_state; // NFC 리더기 상태
 
+    private boolean mScanning; //스캔중인지 FLAG
+    private LeDeviceListAdapter mLeDeviceListAdapter; // 스캔된 bt 기기 리스트를 보여주는 어댑터
+    private BluetoothAdapter mBluetoothAdapter; // 블루투스 어댑터
+    public static final String SERIAL_NUMBER = ""; //특정 리더기만 매칭시키고싶을때 S/N끝자리 숫자 5개를 입력하면 됨.
+    public static Boolean is_live_readeractivity; // 리더 액티비티가 살아있는지 우뮤
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -65,6 +85,7 @@ public class MainActivity extends AppCompatActivity {
 
         init();
         check_token();
+        getPermissionBt();
         setFunction();
     }
 
@@ -80,6 +101,9 @@ public class MainActivity extends AppCompatActivity {
 
         wv_entrance = (WebView) findViewById(R.id.wv01);
         tv_nfc_state = findViewById(R.id.tv_nfc_state);
+        mScanning = false;
+        is_live_readeractivity = false;
+
 
     }
 
@@ -121,6 +145,7 @@ public class MainActivity extends AppCompatActivity {
 
         // NFC리더기 연동상태 확인 핸들러 구현
         h_check_service = new Handler() {
+            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
             public void handleMessage(Message msg) {
 
                 if (msg.what == 0) {
@@ -132,9 +157,22 @@ public class MainActivity extends AppCompatActivity {
                         tv_nfc_state.setVisibility(View.GONE);
                     }
                 }
+
+                if (!nfc_state.equals("GATT_SUCCESS")) {
+                    startScanner();
+                    tv_nfc_state.setBackground(getResources().getDrawable(R.drawable.rounded_squre_gray, null));
+                    tv_nfc_state.setText(" 리더기 OFF ");
+                } else {
+                    stopScanner();
+                    tv_nfc_state.setBackground(getResources().getDrawable(R.drawable.rounded_squre_blue, null));
+                    tv_nfc_state.setText(" 리더기 ON ");
+                }
             }
         };
         h_check_service.sendEmptyMessage(0);
+
+        // NFC 리더기 상태 초기화
+        nfc_state = " NFC Off ";
 
     }
 
@@ -253,4 +291,145 @@ public class MainActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
     }
+
+
+    //스캐터 시작
+    public void startScanner() {
+        mLeDeviceListAdapter = new LeDeviceListAdapter();
+        scanLeDevice(true);
+    }
+
+    //스캐터 종료
+    public void stopScanner() {
+        scanLeDevice(false);
+        if (mLeDeviceListAdapter != null) mLeDeviceListAdapter.clear();
+    }
+
+    //블루투스 스캐너 실행 (true 실행, / false 종료)
+    private synchronized void scanLeDevice(final boolean enable) {
+        if (enable && !mScanning) { //스캔이 필요할 시
+            mScanning = true;
+            mBluetoothAdapter.startLeScan(mLeScanCallback);
+            Log.i(TAG, "scanLeDevice/enable&&!mScanning/nfc_state:" + nfc_state);
+        } else if (enable && mScanning) {// 스캔 중에 스캔해라고 명령이 왔을 시
+            Log.i(TAG, "--scanLeDevice/enable&&mScanning/nfc_state:" + nfc_state);
+
+        } else if (!enable && mScanning) {// 스캔중에 정지 명령이 왔을시
+            Log.i(TAG, "scanLeDevice/!enable&&mScanning/nfc_state:" + nfc_state);
+            mScanning = false;
+            mBluetoothAdapter.stopLeScan(mLeScanCallback);
+        } else if (!enable && !mScanning) { // 스캔하지않을때 스캔중지 명령이 왓을시
+            Log.i(TAG, "--scanLeDevice/!enable&&!mScanning/nfc_state:" + nfc_state);
+        }
+    }
+
+    // bt 리스트 를 관리하는 어댑터
+    /* Adapter for holding devices found through scanning. */
+    private class LeDeviceListAdapter extends BaseAdapter {
+        private ArrayList<BluetoothDevice> mLeDevices;
+        private LayoutInflater mInflator;
+
+        public LeDeviceListAdapter() {
+            super();
+            mLeDevices = new ArrayList<BluetoothDevice>();
+        }
+
+        public void addDevice(BluetoothDevice device) {
+            if (!mLeDevices.contains(device)) {
+                mLeDevices.add(device);
+            }
+        }
+
+        public BluetoothDevice getDevice(int position) {
+            return mLeDevices.get(position);
+        }
+
+        public void clear() {
+            mLeDevices.clear();
+        }
+
+        @Override
+        public int getCount() {
+            return mLeDevices.size();
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return mLeDevices.get(i);
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return i;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            return view;
+        }
+    }
+
+    //블루투스 퍼미션 요청
+    private void getPermissionBt() {
+        final BluetoothManager bluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+
+        mBluetoothAdapter = bluetoothManager.getAdapter();
+
+        if (!getPackageManager().hasSystemFeature(
+                PackageManager.FEATURE_BLUETOOTH_LE)) {
+            Toast.makeText(this, "블루투스를 켜주세요.", Toast.LENGTH_SHORT)
+                    .show();
+            onDestroy();
+        }
+
+        if (!mBluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(
+                    BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            this.startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        } else {
+            /* Request access coarse location permission. */
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                        REQUEST_ACCESS_COARSE_LOCATION);
+            }
+        }
+    }
+
+    // 스캔된 bt리스트 결과를 받는 어댑터
+    /* Device scan callback. */
+    private BluetoothAdapter.LeScanCallback mLeScanCallback = new BluetoothAdapter.LeScanCallback() {
+        @Override
+        public void onLeScan(final BluetoothDevice device, int rssi,
+                             byte[] scanRecord) {
+            //블루투스기기가 스캔은 됐지만 기기이름이 불분명하지 않은경우만 인식시킴
+            if (device.getName() != null) {
+                Log.i(TAG, "device.name:" + device.getName());
+
+                //(중요) NFC 리더기ACR1255의 디바이스네임에는 항상 "ACR1255U-J1-" 이게 붙어있음
+                // 기기별 구분자로 ACR1255U-J1-뒤에 시리얼넘버 5자리가 붙어있음 ex) ACR1255U-J1-12345
+                // 따라서 특정한 리더기만 인식되게 하려면 MainActivity.SERIAL_NUMBER 여기에 리더기 뒤에있는 시리얼넘버를 넣어주면 됨
+                if (device.getName().contains("ACR1255U-J1-" + MainActivity.SERIAL_NUMBER) && !MainActivity.is_live_readeractivity) {
+                    Log.i(TAG, "(sn)Find!~~~ : " + device.toString());
+
+                    //연결 됨
+                    MainActivity.is_live_readeractivity = true;
+                    mLeDeviceListAdapter.addDevice(device); // 연결된 디바이스 어댑터에 등록
+                    mLeDeviceListAdapter.notifyDataSetChanged(); // 어탭터 갱신
+
+                    // NFC리더기가 연결되었으므로 더이상 bt기기를 찾을 필요가없으니 스캔종료
+                    scanLeDevice(false);
+
+                    // NFC리더기 발견했으므로 Reader액티비티로 넘어가기
+                    Intent dialogIntent = new Intent(MainActivity.this, ReaderActivity.class);
+                    dialogIntent.putExtra(ReaderActivity.EXTRAS_DEVICE_NAME, device.getName());
+                    dialogIntent.putExtra(ReaderActivity.EXTRAS_DEVICE_ADDRESS,
+                            device.getAddress());
+                    dialogIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(dialogIntent);
+                }
+            }
+        }
+    };
 }
